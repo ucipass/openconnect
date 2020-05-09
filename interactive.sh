7d18a8b4de3b814c1cb886e17363f1328a3f93c5
ucipass@gmail.com <ucipass@gmail.com>
2020-05-09 14:19:53 -0500

    forwarding fix

--- interactive.sh
+++ interactive.sh
@@ -9,6 +9,11 @@ ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 echo PermitEmptyPasswords yes >> /etc/ssh/sshd_config
 echo PermitRootLogin  yes >> /etc/ssh/sshd_config
 echo PermitEmptyPasswords  yes >> /etc/ssh/sshd_config
+echo AllowTCPForwarding yes  >> /etc/ssh/sshd_config
+echo PermitOpen any >> /etc/ssh/sshd_config
+echo PermitTunnel yes >> /etc/ssh/sshd_config
+sed -i '/AllowTcpForwarding/d' /etc/ssh/sshd_config
+echo AllowTcpForwarding local >> /etc/ssh/sshd_config
 PROXY_PORT=3128 node /root/proxy.js &
 /usr/sbin/sshd
 echo "All services started!"
